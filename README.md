f28893b4671986cd4a0f0b98c0240f61397a237c
Cyclone77 <fillchar@live.com>
2017-07-27 15:05:52 +0800

    给缓存添加过期时间

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-### Storage 缓冲处理
+### Storage 缓存处理
+
+- 2017年7月27日 11:48:17 增加设置缓存（type = 'local'）设置时间
+
+``` javascript
+Store("local").set("Name", "Cy"); //设置key
+Store("local").set("Name", "Cy", 1); //设置过期时间的key
+console.log(Store("local").get("Name"));
+```
--- Store.js
+++ Store.js
@@ -1,22 +1,30 @@
-﻿; (function (window, undefined) {
+﻿;
+(function(window, undefined) {
 
-    var ensure = function (obj, name, factory) {
+    var ensure = function(obj, name, factory) {
         return obj[name] || (obj[name] = factory());
     };
     /*
-    *   创建Storage对象
-    *   typespace { string } 缓存类型
-    */
-    var createStorage = function (typespace) {
+     *   创建Storage对象
+     *   typespace { string } 缓存类型
+     */
+    var createStorage = function(typespace) {
         var StorageInstance = {
             type: (!!typespace ? typespace : "session") + "Storage", //local本地存储，session会话存储
             /*
-            *   保存缓存，页面回话打开期间有效
-            *   @key {String} 键
-            *   @val {String} 值
-            */
-            set: function (key, val) {
-                if (Object.prototype.toString.call(val) != "[object String]") throw "缓存保存值必须是字符串！";
+             *   保存缓存，页面回话打开期间有效
+             *   @key {String} 键
+             *   @val {String} 值
+             *   @expTime {int} 过期时间，不设置为不过期
+             */
+            set: function(key, val, expTime) {
+                var data = {
+                    "ExpirationTime": !!expTime && expTime, //按小时计算
+                    "SetTime": new Date().toString(),
+                    "Value": val
+                };
+                val = JSON.stringify(data);
+                if (Object.prototype.toString.call(val) != "[object String]") throw "'key' is not string！";
                 //各浏览器支持的 localStorage 和 sessionStorage 容量上限不同。
                 //如果达到上限则删除第一个键重新保存
                 try {
@@ -27,32 +35,46 @@
                 }
             },
             /*
-            *   获取键的值
-            *   @key { string } 键名称
-            */
-            get: function (key) {
-                return window[this.type].getItem(key);
+             *   获取键的值
+             *   @key { string } 键名称
+             */
+            get: function(key) {
+                var str = window[this.type].getItem(key),
+                    data = JSON.parse(str),
+                    expTime = data.ExpirationTime,
+                    setTime = new Date(data.SetTime);
+                if (!!expTime && typeof expTime == "number") {
+                    var nowTime = new Date();
+                    if (Math.abs(parseInt(setTime - nowTime) / 1000 / 60 / 60) > expTime) {
+                        console.log("缓存已过期!");
+                        return null;
+                    }
+                    return data.Value;
+                }
+                return data.Value;
             },
             /*
-            *   获取第n个键的值
-            *   index { int } 位置
-            */
-            key: function(index){
-                return window[this.type].key(index);
+             *   获取第n个键的值
+             *   index { int } 位置
+             */
+            key: function(index) {
+                var str = window[this.type].key(index),
+                    data = JSON.parse(str);
+                return data.Value;
             },
             /*
-            *   删除指定key值
-            *   @key {String} 键名称
-            */
-            del: function (key) {
+             *   删除指定key值
+             *   @key {String} 键名称
+             */
+            del: function(key) {
                 window[this.type].removeItem(key);
             },
             //返回StorageStorage对象
-            getStorage: function () {
+            getStorage: function() {
                 return window[this.type];
             },
             //删除当前第一个StorageStorage对象的键
-            shift: function () {
+            shift: function() {
                 this.del(window[this.type].key(0));
             }
         };
@@ -60,13 +82,13 @@
         return StorageInstance;
     }
 
-    ensure(window, "Store", function () {
+    ensure(window, "Store", function() {
         /*
-        *   local本地存储，session会话存储
-        *   typespace {String} local,session
-        */
-        var outStore = function (typespace) {            
-            return createStorage(typespace);
+         *   local本地存储，session会话存储
+         *   typespace {String} local,session
+         */
+        var outStore = function(typespace) {
+            return new createStorage(typespace);
         }
         return outStore;
     });
